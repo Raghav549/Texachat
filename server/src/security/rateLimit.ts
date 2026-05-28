4390f97d6da04b18560159ddc0fbe1e7740c77da
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-28 11:09:28 +0530

    Create rateLimit.ts

---
+++ server/src/security/rateLimit.ts
@@ -0,0 +1,18 @@
+import rateLimit from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    error: "Too many authentication attempts",
+  },
+});
+
+export const apiLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 120,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
